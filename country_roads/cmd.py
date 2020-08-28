7db6145f5fcc4531000b1451c7971fab9527963a
jang <jang@ioctl.org>
2020-08-28 17:20:36 +0100

    Add the fundamental constraint
    This describes the relationship between a cell being "visited" and its
    edges being crossed by the loop.

--- country_roads/cmd.py
+++ country_roads/cmd.py
@@ -1,5 +1,7 @@
 from country_roads.puzzle import Puzzle
 from country_roads.display import plot
+from country_roads.solution import add_constraints
+
 
 def main():
     p = Puzzle(
@@ -13,6 +15,7 @@ def main():
         d=3,
         i=2,
     )
+    add_constraints(p)
     while p.solve():
         plot(p)
 
---
+++ country_roads/solution.py
@@ -0,0 +1,26 @@
+import z3
+
+
+def add_constraints(puzzle):
+    """Add the rules that guide us toward a single solution"""
+
+    loop_logic(puzzle)
+
+
+def loop_logic(puzzle):
+    """Basic connectedness rules:
+       Iff a cell is not visited, then none of its edges are traversed
+       Iff a cell is visited, then precisely two of its borders are traversed
+    """
+    for cell in puzzle.cells:
+        borders = puzzle.borders(cell)
+
+        off = z3.Not(puzzle[cell])
+        puzzle.add(off == z3.And(*(z3.Not(puzzle[border]) for border in borders)))
+
+        bool_to_int = z3.Function("bool_to_int", z3.BoolSort(), z3.IntSort())
+        puzzle.add(bool_to_int(True) == 1)
+        puzzle.add(bool_to_int(False) == 0)
+
+        on = puzzle[cell]
+        puzzle.add(on == (sum(bool_to_int(puzzle[border]) for border in borders) == 2))
